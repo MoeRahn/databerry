20cb567270abb3f452ea99d744768fbf3a50ccee
yahyha benzha <yahya.benzha1@gmail.com>
2024-02-14 15:35:12 +0100

    feat: 🎸 sync url input with fields input

--- apps/dashboard/components/AgentInputs/HttpToolInput.tsx
+++ apps/dashboard/components/AgentInputs/HttpToolInput.tsx
@@ -1,8 +1,10 @@
-import { zodResolver } from '@hookform/resolvers/zod';
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import DeleteIcon from '@mui/icons-material/Delete';
 import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
+import SettingsIcon from '@mui/icons-material/Settings';
 import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
   Alert,
   Button,
   Card,
@@ -18,7 +20,15 @@ import {
   Stack,
   Typography,
 } from '@mui/joy';
-import React, { useCallback, useMemo } from 'react';
+import React, {
+  ElementRef,
+  memo,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import {
   Control,
   FieldValue,
@@ -28,8 +38,9 @@ import {
   useForm,
   useFormContext,
 } from 'react-hook-form';
-import z from 'zod';
 
+import { useDebounce } from '@app/hooks/useDebounce';
+import useDeepCompareEffect from '@app/hooks/useDeepCompareEffect';
 import useModal from '@app/hooks/useModal';
 
 import {
@@ -38,77 +49,377 @@ import {
   ToolSchema,
 } from '@chaindesk/lib/types/dtos';
 
+import { Choices } from '../BlablaFormEditor/FieldsInput';
 import Input from '../Input';
 
-const KeyValueFieldArray = (props: {
+type Origin = 'url' | 'fields';
+export type Fields =
+  | {
+      key: string;
+      value?: string | undefined;
+      isUserProvided?: boolean | undefined;
+      description?: string;
+      acceptedValues?: (string | undefined)[];
+    }[]
+  | undefined;
+
+const KeyValueFieldArray = ({
+  name,
+  label,
+  userOnly = false,
+  prefix,
+}: {
   name:
     | 'config.queryParameters'
+    | 'config.pathVariables'
     | 'config.body'
     | 'config.headers'
     | 'tools.0.config.queryParameters'
+    | 'tools.0.config.pathVariables'
     | 'tools.0.config.body'
     | 'tools.0.config.headers';
   label?: string;
+  userOnly?: boolean;
+  prefix: '' | 'tools.0.';
 }) => {
   const methods = useFormContext<HttpToolSchema | CreateAgentSchema>();
+
   const parameters = useFieldArray({
     control: methods.control as Control<HttpToolSchema>,
-    name: props.name,
+    name,
   });
 
+  // the source of change.
+  const syncOriginRef = useRef<Origin>('url');
+
+  // TODO: fix types issue#53588777
+  const watchedValues = methods.watch([
+    `${prefix}config.url`,
+    `${prefix}config.queryParameters`,
+    `${prefix}config.pathVariables`,
+  ] as any);
+
+  const debouncedWatchedValues = useDebounce(watchedValues, 50);
+
+  /* synchronize state of url with state of query fields */
+  const syncQueryParamsToUrl = (
+    origin: Origin,
+    url: string,
+    fields: Fields
+  ) => {
+    switch (origin) {
+      case 'url':
+        try {
+          let searchParams = new Map(new URL(url).searchParams.entries());
+
+          // No search params, remove all at once, exit early.
+          if (searchParams.size === 0) {
+            parameters.remove();
+            return;
+          }
+
+          if (fields?.length !== searchParams.size) {
+            fields?.forEach((field, index) => {
+              if (!searchParams.has(field.key)) {
+                parameters.remove(index);
+              }
+            });
+          }
+
+          searchParams.forEach((value, key) => {
+            const fieldIndex = fields?.findIndex((field) => field.key === key);
+
+            const payload = {
+              key,
+              ...(value == '{user}'
+                ? {
+                    value: '',
+                    isUserProvided: true,
+                  }
+                : { value, isUserProvided: false }),
+            };
+
+            if (fieldIndex === undefined || fieldIndex == -1) {
+              const indexInUrl = Array.from(searchParams.keys()).indexOf(key);
+              // should not update nested content.
+              const queryParamContent = methods.getValues(
+                `${prefix}config.queryParameters.${indexInUrl}`
+              );
+              parameters.update(indexInUrl, {
+                ...queryParamContent,
+                ...payload,
+              });
+            } else if (value !== fields?.[fieldIndex!].value) {
+              // should not update nested content.
+              const queryParamContent = methods.getValues(
+                `${prefix}config.queryParameters.${fieldIndex}`
+              );
+
+              parameters.update(fieldIndex, {
+                ...queryParamContent,
+                ...payload,
+              });
+            }
+          });
+        } catch (e) {
+          if (url === '') {
+            parameters.remove();
+          }
+        }
+        break;
+      case 'fields':
+        try {
+          const Url = new URL(url);
+          Url.search = '';
+          fields?.forEach((field) => {
+            if (!(field.value == '' && !field.isUserProvided)) {
+              Url.searchParams.set(
+                field.key,
+                field.isUserProvided ? '{user}' : field.value || ''
+              );
+            }
+          });
+          methods.setValue(`${prefix}config.url`, decodeURI(Url.toString()), {
+            shouldValidate: true,
+            shouldDirty: true,
+          });
+        } catch (e) {
+          let newUrl = '';
+          fields?.forEach((field, index) => {
+            // should not add query param if no value.
+            if (field.value == '' && !field.isUserProvided) return;
+
+            newUrl += `${index === 0 ? '?' : '&'}${field.key}=${
+              field.isUserProvided ? '{user}' : field.value
+            }`;
+          });
+
+          // let's not cause a re-render if no change.
+          if (newUrl !== url) {
+            methods.setValue(`${prefix}config.url`, newUrl, {
+              shouldValidate: true,
+              shouldDirty: true,
+            });
+          }
+        }
+        break;
+      default:
+        throw new Error(`Uknown Origin: ${syncOriginRef.current}`);
+    }
+  };
+
+  const syncPathVariablesToUrl = (
+    origin: Origin,
+    url: string,
+    fields: Fields
+  ) => {
+    switch (origin) {
+      case 'url':
+        const pathVariablesFromUrl =
+          url
+            ?.split('?')[0]
+            ?.match(/:([\w-]+)/g)
+            ?.map((param) => param.substring(1)) || [];
+
+        // clear route params fields. exit.
+        if (pathVariablesFromUrl.length == 0) {
+          parameters.remove();
+          return;
+        }
+
+        fields?.forEach((field, index) => {
+          if (
+            !pathVariablesFromUrl.includes(field.key) &&
+            pathVariablesFromUrl.length !== parameters.fields.length
+          ) {
+            parameters.remove(index);
+          }
+        });
+
+        pathVariablesFromUrl.forEach((param, index) => {
+          // should not update nested content.
+          const pathVariableContent = methods.getValues(
+            `${prefix}config.pathVariables.${index}`
+          );
+
+          parameters.update(index, {
+            ...pathVariableContent,
+            key: param,
+            isUserProvided: true,
+          });
+        });
+        break;
+      case 'fields':
+        try {
+          const Url = new URL(url);
+          Url.pathname = Url.pathname.replace(/\/+$/, '');
+          let basePath =
+            Url.origin +
+            Url.pathname
+              .split('/')
+              .filter((p) => !p.startsWith(':'))
+              .join('/');
+
+          if (fields?.length == 0) {
+            methods.setValue(
+              `${prefix}config.url`,
+              `${basePath}${Url.search}`,
+              {
+                shouldValidate: false,
+                shouldDirty: false,
+              }
+            );
+          }
+          const queryPaths = fields
+            ?.map((field) => field.key)
+            .filter((key) => key !== '');
+
+          queryPaths?.forEach((path) => (basePath += `/:${path}`));
+
+          if (basePath !== url) {
+            methods.setValue(
+              `${prefix}config.url`,
+              `${basePath}${Url.search}`,
+              {
+                shouldValidate: false,
+                shouldDirty: false,
+              }
+            );
+          }
+        } catch (e) {
+          if (fields) {
+            const pathVariables = fields
+              .map((field) => `/:${field.key}`)
+              .join('');
+            methods.setValue(`${prefix}config.url`, pathVariables, {
+              shouldValidate: false,
+              shouldDirty: false,
+            });
+          }
+        }
+        break;
+      default:
+        throw new Error(`Uknown Origin: ${syncOriginRef.current}`);
+    }
+  };
+
+  // synchronize state of url field with state query and paths array fields.
+  useDeepCompareEffect(() => {
+    const [debouncedUrl, debouncedParamFields, debouncedPathVariablesFields] =
+      debouncedWatchedValues;
+
+    if (name.includes('queryParameters')) {
+      syncQueryParamsToUrl(
+        syncOriginRef.current,
+        debouncedUrl,
+        debouncedParamFields
+      );
+    } else if (name.includes('pathVariables')) {
+      syncPathVariablesToUrl(
+        syncOriginRef.current,
+        debouncedUrl,
+        debouncedPathVariablesFields
+      );
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedWatchedValues, syncOriginRef.current]);
+
   return (
-    <FormControl>
-      {props.label && <FormLabel>{props.label}</FormLabel>}
+    <FormControl
+      onMouseDown={() => {
+        syncOriginRef.current = 'fields';
+      }}
+      onMouseLeave={() => {
+        syncOriginRef.current = 'url';
+      }}
+    >
+      {label && <FormLabel>{label}</FormLabel>}
       <Stack gap={1}>
         {parameters.fields.map((field, index) => (
-          <Stack key={field.id} direction="row" gap={1} alignItems={'end'}>
-            <Input
-              control={methods.control}
-              placeholder="Key"
-              {...methods.register(`${props.name}.${index}.key` as const)}
-            />
+          <Stack key={field.id} gap={1}>
+            <Stack direction="row" gap={1} alignItems={'end'}>
+              <Input
+                control={methods.control}
+                placeholder="Key"
+                {...methods.register(`${name}.${index}.key` as any)}
+              />
 
-            <Input
-              control={methods.control}
-              placeholder="Value"
-              {...methods.register(`${props.name}.${index}.value` as const)}
-              disabled={!!field.isUserProvided}
-            />
-
-            <FormControl>
-              <Card variant="outlined" size="sm">
-                <Checkbox
-                  size="sm"
-                  label="Provided By User"
-                  checked={!!field.isUserProvided}
-                  slotProps={{
-                    label: {
-                      sx: {
-                        whiteSpace: 'nowrap',
+              <Input
+                control={methods.control}
+                placeholder="Value"
+                {...methods.register(`${name}.${index}.value` as any)}
+                disabled={!!field.isUserProvided}
+              />
+              <FormControl>
+                <Card variant="outlined" size="sm">
+                  <Checkbox
+                    size="sm"
+                    label="Provided By User"
+                    checked={!!field.isUserProvided}
+                    slotProps={{
+                      label: {
+                        sx: {
+                          whiteSpace: 'nowrap',
+                        },
                       },
-                    },
-                  }}
-                  onChange={(e) => {
-                    const fieldValues = methods.getValues(
-                      `${props.name}.${index}`
-                    );
-                    parameters.update(index, {
-                      ...fieldValues,
-                      value: '',
-                      isUserProvided: e.target.checked,
-                    });
-                  }}
-                />
-              </Card>
-            </FormControl>
+                    }}
+                    onChange={(e) => {
+                      const fieldValues = methods.getValues(`${name}.${index}`);
+                      parameters.update(index, {
+                        ...fieldValues,
+                        value: '',
+                        isUserProvided: e.target.checked,
+                      });
+                    }}
+                  />
+                </Card>
+              </FormControl>
 
-            <IconButton
-              variant="outlined"
-              color="neutral"
-              onClick={() => parameters.remove(index)}
-            >
-              <DeleteIcon />
-            </IconButton>
+              <IconButton
+                variant="outlined"
+                color="neutral"
+                onClick={() => parameters.remove(index)}
+              >
+                <DeleteIcon />
+              </IconButton>
+            </Stack>
+            {field.isUserProvided && (
+              <Accordion sx={{ ml: 1 }}>
+                <AccordionSummary>
+                  <Typography
+                    level="body-sm"
+                    color="primary"
+                    startDecorator={
+                      <SettingsIcon fontSize="sm" color="primary" />
+                    }
+                  >
+                    Advanced
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Stack gap={2}>
+                    <Divider />
+                    <Input
+                      control={methods.control}
+                      label="Describe your key usage for better AI inference :"
+                      placeholder="Description"
+                      {...methods.register(
+                        `${name}.${index}.description` as any
+                      )}
+                    />
+
+                    <Choices<HttpToolSchema | CreateAgentSchema>
+                      actionLabel="Add A Value"
+                      name={`${name}.${index}.acceptedValues`}
+                      init={false}
+                      label="Specify the accepted values for the key :"
+                    />
+                    <Divider sx={{ my: 2 }} />
+                  </Stack>
+                </AccordionDetails>
+              </Accordion>
+            )}
           </Stack>
         ))}
         <Button
@@ -118,6 +429,7 @@ const KeyValueFieldArray = (props: {
             parameters.append({
               key: '',
               value: '',
+              ...(userOnly ? { isUserProvided: userOnly } : {}),
             })
           }
         >
@@ -132,14 +444,23 @@ type Props = {
   name?: 'tools.0';
 };
 
-export default function HttpToolInput({ name }: Props) {
+function HttpToolInput({ name }: Props) {
   const methods = useFormContext<HttpToolSchema | CreateAgentSchema>();
-  const prefix = useMemo(() => {
-    return name ? `${name}.` : '';
-  }, [name]) as 'tools.0.' | '';
+  const prefix: 'tools.0.' | '' = name ? `${name}.` : '';
   const templatesModal = useModal();
   const [withApprovalChecked] = methods.watch([`${prefix}config.withApproval`]);
   const [methodValue] = methods.watch([`${prefix}config.method`]);
+
+  // Fallback request method to GET.
+  useEffect(() => {
+    const requestMethod = methods.getValues(`${prefix}config.method`);
+    if (!requestMethod) {
+      methods.setValue(`${prefix}config.method`, 'GET', {
+        shouldValidate: true,
+        shouldDirty: true,
+      });
+    }
+  }, [methods, prefix]);
 
   return (
     <Stack>
@@ -216,17 +537,34 @@ export default function HttpToolInput({ name }: Props) {
           </Select>
         </FormControl>
 
-        <KeyValueFieldArray label="Headers" name={`${prefix}config.headers`} />
+        <KeyValueFieldArray
+          label="Path Variables"
+          prefix={prefix}
+          name={`${prefix}config.pathVariables`}
+          userOnly
+        />
+
+        <KeyValueFieldArray
+          label="Headers"
+          prefix={prefix}
+          name={`${prefix}config.headers`}
+        />
 
         <KeyValueFieldArray
           label="Query Parameters"
+          prefix={prefix}
           name={`${prefix}config.queryParameters`}
         />
 
-        <KeyValueFieldArray
-          label="Body Parameters"
-          name={`${prefix}config.body`}
-        />
+        {!['GET', 'DELETE'].includes(
+          methods.getValues(`${prefix}config.method`)
+        ) && (
+          <KeyValueFieldArray
+            label="Body Parameters"
+            prefix={prefix}
+            name={`${prefix}config.body`}
+          />
+        )}
 
         {/* <Card size="sm" variant="outlined"> */}
         <FormControl>
@@ -277,6 +615,7 @@ export default function HttpToolInput({ name }: Props) {
                       name: 'Random Cat Image',
                       description: 'Useful for getting a random cat image',
                       url: 'https://api.thecatapi.com/v1/images/search',
+                      method: 'GET',
                       headers: [],
                       queryParameters: [],
                       body: [],
@@ -299,3 +638,5 @@ export default function HttpToolInput({ name }: Props) {
     </Stack>
   );
 }
+
+export default memo(HttpToolInput);
